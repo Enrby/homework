ba313dd44c3adf2c459966a07cc4d26957e27252
Максим <utkin.maxim@mail.ru>
2023-02-11 21:13:40 +0300

    DZ done

---
+++ src/object/Main.java
@@ -0,0 +1,18 @@
+package object;
+
+public class Main {
+    public static void main(String[] args) {
+        Autor dostoevsky = new Autor("Федор", "Достоевский");
+        Autor tolstoy = new Autor("Лев", "Толстой");
+
+        Book Crime = new Book("Преступление и наказание", 1866, dostoevsky);
+        Book War = new Book("Война и мир", 1867, tolstoy);
+
+        Crime.printInfo();
+        War.printInfo();
+
+        Crime.setPublishingYear(1870);
+
+        Crime.printInfo();
+    }
+}
